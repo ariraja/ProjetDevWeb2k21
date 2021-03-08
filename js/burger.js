c2840351705376656e731f896cb6a0088fa04733
Jonathan <jonathanlouamba@gmail.com>
2021-03-08 21:28:53 +0100

    ajout bouton -/+ et js que sur la page burger

---
+++ js/burger.js
@@ -0,0 +1,69 @@
+function plus1(){
+    if(n.value != 10){
+    n = document.getElementById("qte1");
+    n.value = parseInt(n.value)+1;
+    }
+
+}
+
+function plus2(){
+    if(n.value != 10){
+    n = document.getElementById("qte2");
+    n.value = parseInt(n.value)+1;
+    }
+}
+
+function plus3(){
+ 
+    n = document.getElementById("qte3");
+    n.value = parseInt(n.value)+1;
+    
+}
+
+function plus4(){
+    if(n.value != 10){
+    n = document.getElementById("qte4");
+    n.value = parseInt(n.value)+1;
+    }
+}
+
+function plus5(){
+    n = document.getElementById("qte5");
+    n.value = parseInt(n.value)+1;
+
+}
+
+function moins1(){
+    if(n.value != 0){
+    n = document.getElementById("qte1");
+    n.value = parseInt(n.value)-1;
+    }
+}
+
+function moins2(){
+    if(n.value != 0){
+    n = document.getElementById("qte2");
+    n.value = parseInt(n.value)-1;
+    }
+}
+
+function moins3(){
+    if(n.value != 0){
+    n = document.getElementById("qte3");
+    n.value = parseInt(n.value)-1;
+    }
+}
+
+function moins4(){
+    if(n.value != 0){
+    n = document.getElementById("qte4");
+    n.value = parseInt(n.value)-1;
+    }
+}
+
+function moins5(){
+    if(n.value != 0){
+    n = document.getElementById("qte5");
+    n.value = parseInt(n.value)-1;
+    }
+}
