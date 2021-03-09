7cb4e38ded227ca273b7d1f4e2976804f4b7d002
ariraja <arihoela@gmail.com>
2021-03-09 22:37:30 +0100

    stock + boutons fini

--- js/burger.js
+++ js/burger.js
@@ -1,75 +1,136 @@
 function plus1(){
     n = document.getElementById("qte1");
-    if(n.value < 11){
-    n.value++;
+    st=document.getElementsByClassName("stock")[0];
+    p = document.getElementById("plus1");
+    m = document.getElementById("moins1");
+    if(n.value < parseInt(st.textContent)){
+        n.value++;
+        m.disabled=false;
+    }
+    if(n.value == parseInt(st.textContent)){
+        p.disabled = true;
     }
 }
 
 function plus2(){
     n = document.getElementById("qte2");
-    if(n.value < 11){
-    n.value++;
+    st=document.getElementsByClassName("stock")[1];
+    p = document.getElementById("plus2");
+    m = document.getElementById("moins2");
+    if(n.value < parseInt(st.textContent)){
+        n.value++;
+        m.disabled=false;
+    }
+    if(n.value == parseInt(st.textContent)){
+        p.disabled = true;
     }
 }
 
 function plus3(){
     n = document.getElementById("qte3");
-    if(n.value < 11){
-    n.value++;
+    st=document.getElementsByClassName("stock")[2];
+    p = document.getElementById("plus3");
+    m = document.getElementById("moins3");
+    if(n.value < parseInt(st.textContent)){
+        n.value++;
+        m.disabled=false;
+    }
+    if(n.value == parseInt(st.textContent)){
+        p.disabled = true;
     }
 }
 
 function plus4(){
     n = document.getElementById("qte4");
-    if(n.value < 11){
-    n.value++;
+    st=document.getElementsByClassName("stock")[3];
+    p = document.getElementById("plus4");
+    m = document.getElementById("moins4");
+    if(n.value < parseInt(st.textContent)){
+        n.value++;
+        m.disabled=false;
+    }
+    if(n.value == parseInt(st.textContent)){
+        p.disabled = true;
     }
 }
 
 function plus5(){
     n = document.getElementById("qte5");
-    if(n.value < 11){
-    n.value++;
+    st=document.getElementsByClassName("stock")[4];
+    p = document.getElementById("plus5");
+    m = document.getElementById("moins5");
+    if(n.value < parseInt(st.textContent)){
+        n.value++;
+        m.disabled=false;
+    }
+    if(n.value == parseInt(st.textContent)){
+        p.disabled = true;
     }
 }
 
 function moins1(){
     n = document.getElementById("qte1");
+    p = document.getElementById("plus1");
+    m = document.getElementById("moins1");
     if(n.value > 0){
-    n.value--;
+        p.disabled = false;
+        n.value--;
     }
-//moins = document.getElementsByClassName("moins1");
-//if(n.value == 0){
-//    moins.disabled = true;
-//    console.log('*');
-//}
+    if(n.value == 0){
+        m.disabled = true;
+    }
+
 }
 
 function moins2(){
-n = document.getElementById("qte2");
-if(n.value > 0){
-n.value--;
-}
+    n = document.getElementById("qte2");
+    p = document.getElementById("plus2");
+    m = document.getElementById("moins2");
+    if(n.value > 0){
+        p.disabled = false;
+        n.value--;
+    }
+    if(n.value == 0){
+        m.disabled = true;
+    }
 }
 
 function moins3(){
-n = document.getElementById("qte3");
-if(n.value > 0){
-n.value--;
-}
+    n = document.getElementById("qte3");
+    p = document.getElementById("plus3");
+    m = document.getElementById("moins3");
+    if(n.value > 0){
+        p.disabled = false;
+        n.value--;
+    }
+    if(n.value == 0){
+        m.disabled = true;
+    }
 }
 
 function moins4(){
-n = document.getElementById("qte4");
-if(n.value > 0){
-n.value--;
-}
+    n = document.getElementById("qte4");
+    p = document.getElementById("plus4");
+    m = document.getElementById("moins4");
+    if(n.value > 0){
+        p.disabled = false;
+        n.value--;
+    }
+    if(n.value == 0){
+        m.disabled = true;
+    }
 }
 
 function moins5(){
     n = document.getElementById("qte5");
+    p = document.getElementById("plus5");
+    m = document.getElementById("moins5");
     if(n.value > 0){
-    n.value--;
+        p.disabled = false;
+        n.value--;
+    }
+    if(n.value == 0){
+        m.disabled = true;
     }
 }
 
