0d58399a57f217461337554e1b021c4517aba1a4
Jonathan <jonathanlouamba@gmail.com>
2021-03-11 10:31:10 +0100

    popup

--- js/burger.js
+++ js/burger.js
@@ -141,8 +141,15 @@ function suppStock(){
     }
 }
 
-function commande(){
-    prompt('wsh');
+function openForm() {
+document.getElementById("popupForm").style.display = "block";
+document.getElementsByClassName("overlay")[0].style.display = "block";
+bg = document.getElementsByClassName("main-content")[0].style;
+}
+
+function closeForm() {
+document.getElementById("popupForm").style.display = "none";
+document.getElementsByClassName("overlay")[0].style.display = "none";
 }
 
 
