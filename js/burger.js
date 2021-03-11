dcc5e94615d79dbee3adf71c78415d2a5260af70
ariraja <arihoela@gmail.com>
2021-03-11 12:04:49 +0100

    maj img

--- js/burger.js
+++ js/burger.js
@@ -158,17 +158,6 @@ function rajouteStock(){
 }
 
 
-function openForm() {
-document.getElementById("popupForm").style.display = "block";
-document.getElementsByClassName("overlay")[0].style.display = "block";
-bg = document.getElementsByClassName("main-content")[0].style;
-}
-
-function closeForm() {
-document.getElementById("popupForm").style.display = "none";
-document.getElementsByClassName("overlay")[0].style.display = "none";
-}
-
 
 
 
---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+function openForm() {
+document.getElementById("popupForm").style.display = "block";
+document.getElementsByClassName("overlay")[0].style.display = "block";
+bg = document.getElementsByClassName("main-content")[0].style;
+}
+
+function closeForm() {
+document.getElementById("popupForm").style.display = "none";
+document.getElementsByClassName("overlay")[0].style.display = "none";
+}
