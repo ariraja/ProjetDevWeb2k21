ac48e9eb2e40342b230b626398cb4e3bfbf0e0d7
ariraja <arihoela@gmail.com>
2021-03-18 14:09:04 +0100

    maj popup

--- js/index.js
+++ js/index.js
@@ -8,3 +8,25 @@ function closeForm() {
 document.getElementById("popupForm").style.display = "none";
 document.getElementsByClassName("overlay")[0].style.display = "none";
 }
+
+
+function Verif_login(){
+   //Validation du mail
+    let mail = document.getElementById("email");
+    let missMail = document.getElementById("email_manquant");
+    let mailValid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (mail.value.length==0) { //si champ nom vide
+        missMail.textContent = 'Entrez votre mail !';
+        missMail.style.color = 'red';
+        Ok = false;
+    }
+    else if (mailValid.test(mail.value) == false) { 
+        missMail.textContent = "L'email doit s'écrire comme sous cette forme : monmail@mail.com";
+        missMail.style.color = "red";
+        Ok = false;
+    }
+    else{
+        missMail.style.display = 'none';
+    }
+    return true;
+}
